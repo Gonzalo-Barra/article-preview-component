0657e85bcd7763a986a7b4b3e02a293dc33444cc
Gonzalo-Barra <gonzalobarravalle@gmail.com>
2022-12-22 00:47:13 -0300

    mobile view done

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,74 @@
 <template lang="">
-  <div>
-    <h1>hola</h1>
-  </div>
+  <main>
+    <div class="card">
+      <div >
+        <img class="image" src="/images/drawers.jpg" alt="flowers" />
+      </div>
+      <div class="text">
+        <h1>
+          Shift the overall look and feel by adding these wonderful touches to
+          furniture in your home
+        </h1>
+        <p>
+          Ever been in a room and felt like something was missing? Perhaps it
+          felt slightly bare and uninviting. I’ve got some simple tips to help
+          you make any room feel complete.
+        </p>
+      </div>
+      <buttonCard />
+    </div>
+
+  </main>
 </template>
-<script>
-export default {
+<script setup>
+
+import buttonCard from './components/buttonCard.vue'
+
+
+</script>
+<style >
+main{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f0f0f0;
+}
+.card{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  border-radius: 15px;
+  width: 325px;
+
+}
+.image{
+  width: 100%;
+  height: auto;
+  border-radius: 15px 15px 0 0;
+}
+.text{
+  padding:30px 37px 30px 37px;
+  Font-size: 15px;
+}
+
+.text h1{
+  color:  hsl(217, 19%, 35%);
+  font-weight: 700;
+  Font-size: 15px;
+  font-family: 'Manrope', sans-serif;
+  padding-bottom: 25px;
   
 }
-</script>
-<style lang="">
+
+.text p{
+  color: hsl(214, 17%, 51%);
+  Font-size: 13px;
+  font-weight: 500;
+  font-family: 'Manrope', sans-serif;
   
+}
+
 </style>
---
+++ src/components/buttonCard.vue
@@ -0,0 +1,132 @@
+<template lang="">
+  <div class="button-class">
+    <div class="button-class--passive" v-if="ocultartDiv">
+    <div class="button-class--passive-info" >  <img class="button-class--passive-avatar" src="/images/avatar.jpg" alt="avatar" />
+      <p class="button-class--passive-infotext"><strong>Michelle Appleton</strong> <br/>28 jun 2020</p></div>
+      <img
+        class="button-share"
+        @click="cambiarMostrarDiv"
+        src="/images/icon-share.svg"
+        alt="button share"
+      />
+    </div>
+    <div class="button-class--active" v-if="mostrarDiv">
+      <p>share</p>
+      <div><img  class="button-class--active-icons" src="/images/icon-facebook.svg" alt="icon facebook" />
+      <img class="button-class--active-icons" src="/images/icon-twitter.svg" alt="icon twitter" />
+      <img class="button-class--active-icons" src="/images/icon-pinterest.svg" alt="icon pinterest" /></div>
+      <img
+        class="button-share"
+        @click="cambiarMostrarDiv"
+        src="/images/icon-share.svg"
+        alt="button share"
+      />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      mostrarDiv: false,
+      ocultartDiv: true,
+    };
+  },
+  methods: {
+    cambiarMostrarDiv() {
+      this.mostrarDiv = !this.mostrarDiv;
+      this.ocultartDiv = !this.ocultartDiv;
+    },
+  },
+};
+</script>
+
+<style>
+.button-class {
+
+  width: 100%;
+  display: flex;
+  
+}
+
+.button-share{
+  width:30px;
+  height: 30px;
+  margin-left: 40px ;
+  padding: 9px;
+  border-radius: 100%;
+background-color: hsl(210, 46%, 95%);
+
+}
+
+.button-class--passive {
+  border-radius: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+  background-color: white;
+
+}
+.button-class--passive-info{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+}
+
+.button-class--passive-infotext{
+  color: hsl(0, 0%, 59%);
+  font-size: 12px;
+  margin-left: 15px;
+  font-family: 'Manrope', sans-serif;
+  font-weight: 500;
+
+}
+.button-class--passive-infotext strong{
+  color:  hsl(214, 17%, 51%);
+  font-weight: 700;
+
+}
+.button-class--passive-avatar {
+  width: 40px;
+  height: 40px;
+
+  border-radius: 100%;
+
+}
+
+.button-class--active {
+
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+  background-color: hsl(217, 19%, 35%);
+}
+
+.button-class--active img {
+  width: 30px;
+  height: 30px;
+  margin: 10px;
+  
+}
+
+.button-class--active p {
+  color: hsl(212, 23%, 69%);
+  font-size: 12px;
+  font-family: 'Manrope', sans-serif;
+  font-weight:5 00;
+}	
+
+.button-class--active .button-class--active-icons{
+  width: 20px;
+  height: 20px;
+
+
+
+}	
+
+</style>
